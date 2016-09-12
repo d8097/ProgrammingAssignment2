d389e00b0b65efeee507d2305f14e9a54cd627e7
DML <kidsandi@sbcglobal.net>
2016-09-11 23:41:45 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is not my work. In an attempt to learn more, I copied it from here:
+## http://stackoverflow.com/questions/33738820/r-programming-cache-the-inverse-of-a-matrix
 
-## Write a short comment describing this function
+## x and inv are stored in the environment within which they are defined.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function() inv <<- solve(x)
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This is not my work. In an attempt to learn more, I copied it from here:
+## http://stackoverflow.com/questions/33738820/r-programming-cache-the-inverse-of-a-matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+cacheSolve <- makeCacheMatrix()
+cacheSolve$set(matrix(1:4, 2))
+cacheSolve$get()
+
+
+funs$setInverse()
+funs$getInverse()
